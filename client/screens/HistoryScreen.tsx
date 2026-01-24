93939891816a2782fabe8aec1f0e189c68f064e8
semih007 <134814989+semih007@users.noreply.github.com>
2026-01-24 12:11:35 +0300

    Adjust paddingTop in HistoryScreen component

--- client/screens/HistoryScreen.tsx
+++ client/screens/HistoryScreen.tsx
@@ -115,7 +115,7 @@ const styles = StyleSheet.create({
   },
   scrollContent: {
     padding: Spacing.lg,
-    paddingTop: 79,
+    paddingTop: 83,
     paddingBottom: 20,
   },
   emptyContainer: {
